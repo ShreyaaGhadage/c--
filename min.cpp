10820ef74123092ec747fb3e66277da0465a9d25
ShreyaaGhadage <shreyaghadage27@gmail.com>
2024-06-05 19:18:48 +0530

    commit

--- min.cpp
+++ min.cpp
@@ -4,10 +4,12 @@ int main(){
 	int arr[]={5,-2,0,3,4,-4,1};
 	int n=sizeof(arr)/sizeof(arr[0]);
 	int mn=INT_MAX;
+	int minidex=-1;
 	for(int i=0;i<n;i++){
 	  if(mn>arr[i]){
 	  	mn=arr[i];
+	  	minidex=i;
 	  }
 }
-cout<<mn;
+cout<<mn<<" "<<minidex;
 }
