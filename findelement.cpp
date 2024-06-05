76dd777c37d4a967a5e06e645f711a5d2e51d280
ShreyaaGhadage <shreyaghadage27@gmail.com>
2024-06-05 16:51:36 +0530

    commit

--- findelement.cpp
+++ findelement.cpp
@@ -1,24 +1,35 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int findelement(int arr[],int n,int key){
-	int i;
-	for(int i=0;i<n;i++)
-	if(arr[i]==key)
-	return i;
-	else
-	return -1;
+
+int findelement(int arr[], int n, int key) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == key)
+            return i;
+    } 
+    return -1;
 }
- int main(){
- 	int n;
- 	cout<<"Enter a number : ";
- 	cin>>n;
- 	int key;
- 	cout<<"Enter a key : ";
- 	cin>>key;
- 	int position = findElement(arr,n,key);
- 	  if(position==-1)
- 	  cout<<"Element no foun : ";
- 	  else
- 	  cout<<"Element find at position "<<position+1;
- 	
- }
+
+int main() {
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+    
+    int arr[n];
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    
+    int key;
+    cout << "Enter a key: ";
+    cin >> key;
+    
+    int position = findelement(arr, n, key);
+    
+    if (position == -1)
+        cout << "Element not found" << endl;
+    else
+        cout << "Element found at position " << position  << endl;
+    
+    return 0;
+}
