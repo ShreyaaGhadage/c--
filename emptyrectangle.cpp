ff508d5824e0760f10c8214347ad905a7ceaa4d5
ShreyaaGhadage <shreyaghadage27@gmail.com>
2024-06-08 19:10:14 +0530

    commit

--- emptyrectangle.cpp
+++ emptyrectangle.cpp
@@ -10,11 +10,17 @@ int main(){
 	
 	   for(int i=1;i<=n;i++){
 		for(int j=1;j<=m;j++){
-			if(i==n || j==m);
-			cout<<"#"<<" " ;
+			if(i==1 || i==n){
+			cout<<"#"<<" ";
+		}
+				else if(j==1 || j==m){
+			cout<<"#"<<" ";
+		}
+			else{
+			cout<<" "<<" ";
+		}
 		}
 		cout<<endl;
 }
 		
 	}
-
